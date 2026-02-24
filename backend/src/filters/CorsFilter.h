6c0e1d7470d52f72e76d3d42523cdbd3c2e54689
“suryansh <suryanshv.ug23.ee@nitp.ac.in>
2026-02-25 02:37:39 +0530

    Add Users model generated by drogon_ctl
    - Introduced a new Users.h file containing the Users model for the ieee_conference_db.
    - Defined columns, constructors, and methods for handling user data.
    - Implemented JSON validation and conversion methods.
    - Added SQL query generation for inserting, finding, and deleting users by primary key.

---
+++ backend/src/filters/CorsFilter.h
@@ -0,0 +1,21 @@
+/**
+ *
+ *  CorsFilter.h
+ *
+ */
+
+#pragma once
+
+#include <drogon/HttpFilter.h>
+using namespace drogon;
+
+
+class CorsFilter : public HttpFilter<CorsFilter>
+{
+  public:
+    CorsFilter() {}
+    void doFilter(const HttpRequestPtr &req,
+                  FilterCallback &&fcb,
+                  FilterChainCallback &&fccb) override;
+};
+
