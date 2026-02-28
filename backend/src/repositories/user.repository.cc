4ac71079781fe7c0561c5cb65012b09f8a94b11b
“suryansh <suryanshv.ug23.ee@nitp.ac.in>
2026-02-28 19:52:27 +0530

    feat: implement user repository methods for CRUD operations

--- backend/src/repositories/user.repository.cc
+++ backend/src/repositories/user.repository.cc
@@ -1,6 +1,7 @@
 #include"./user.repository.hpp"
 #include"../models/Users.h"
 #include"../config/database.h"
+
 using namespace drogon_model::ieee_conference_db;
 using namespace drogon::orm;
 
@@ -20,5 +21,82 @@ int UserRepository::createUser(const user& u){
     {
         std::cerr << e.what() << '\n';
         return -1;
+    }
+}
+
+std::vector<user> UserRepository::getUsers(int page,int limit){
+    try
+    {
+        std::vector<user> users;
+        Mapper<Users> mapper(Database::getClient());
+        auto dbUsers=mapper.limit(limit).offset((page-1)*limit).findAll();
+        for(const auto& dbUser:dbUsers){
+            user u;
+            u.id=dbUser.getValueOfId();
+            u.name=dbUser.getValueOfUsername();
+            u.email=dbUser.getValueOfEmail();
+            u.passwordHash=dbUser.getValueOfPasswordHash();
+            u.role=dbUser.getValueOfRole();
+            users.push_back(u);
+        }
+        return users;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+        return {};
+    }
+}
+
+user UserRepository::getUser(int id){
+    try
+    {
+        Mapper<Users> mapper(Database::getClient());
+        auto dbUser=mapper.findByPrimaryKey(id);
+        user u;
+        u.id=dbUser.getValueOfId();
+        u.name=dbUser.getValueOfUsername();
+        u.email=dbUser.getValueOfEmail();
+        u.passwordHash=dbUser.getValueOfPasswordHash();
+        u.role=dbUser.getValueOfRole();
+        return u;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+        return {};
+    }
+}
+
+bool UserRepository::updateUser(int id,const user& u){
+    try
+    {
+        Mapper<Users> mapper(Database::getClient());
+        auto dbUser=mapper.findByPrimaryKey(id);
+        dbUser.setUsername(u.name);
+        dbUser.setEmail(u.email);
+        dbUser.setPasswordHash(u.passwordHash);
+        dbUser.setRole(u.role);
+        mapper.update(dbUser);
+        return true;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+        return false;
+    }
+}
+
+bool UserRepository::deleteUser(int id){
+    try
+    {
+        Mapper<Users> mapper(Database::getClient());
+        mapper.deleteByPrimaryKey(id);
+        return true;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+        return false;
     }
 }
